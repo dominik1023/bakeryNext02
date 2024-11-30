8a2b6e9c403d22bca90bd3a04c91eaf8ba706efa
dominik1023 <dominik1023@gmail.com>
2024-11-29 21:59:17 -0600

    font-size adjusted

--- src/pages/components/MenuItem.js
+++ src/pages/components/MenuItem.js
@@ -10,7 +10,11 @@ export default function MenuItem({ imgSrc, productName, description }) {
         }}
       >
         <div className="flex-grow">
-            <h2 className="text-lg font-semibold text-gray-900">
+            <h2 className="text-lg font-semibold text-gray-900"
+                style={{
+                    fontSize: "1.75rem"
+                }}
+            >
             {safeProductName}
             </h2>
             <p className="text-sm text-gray-700">{safeDescription}</p>
